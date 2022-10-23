2898507e3d53cea6ed0725e06c0605e9deeab809
TimoshRR <109026289+TimoshRR@users.noreply.github.com>
2022-10-23 23:13:16 +0530

    Merge sort

--- Main.py
+++ Main.py
@@ -2,6 +2,34 @@ from typing import List
 
 def merge_sort(data) -> None:
   # Write code here
+  if len(arr) > 1:
+ 
+         # Finding the mid of the array
+        mid = len(arr)//2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        mergeSort(L)
+ 
+        # Sorting the second half
+        mergeSort(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
 
 
 # Do not change the following code
