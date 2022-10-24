61b8642ed4314cacfffe1864c4cf772c3dab75aa
TimoshRR <109026289+TimoshRR@users.noreply.github.com>
2022-10-24 23:10:20 +0530

    Merge sort

--- Main.py
+++ Main.py
@@ -1,34 +1,43 @@
 from typing import List
 
-def merge_sort(arr) -> None:
-  # Write code here
-  if len(arr) > 1:
- 
-         # Finding the mid of the array
-        mid = len(arr)//2
- 
-        # Dividing the array elements
-        L = arr[:mid]
- 
-        # into 2 halves
-        R = arr[mid:]
- 
-        # Sorting the first half
-        mergeSort(L)
- 
-        # Sorting the second half
-        mergeSort(R)
- 
-        i = j = k = 0
- 
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i += 1
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
             else:
-                arr[k] = R[j]
+                myList[k] = right[j]
                 j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
             k += 1
 
 
